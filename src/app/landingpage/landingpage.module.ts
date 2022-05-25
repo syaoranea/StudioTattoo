596f698add933af1b1ff55eaa453515d4b923927
Eduardo Rocha <eduardo_rocha87@hotmail.com>
2022-05-25 12:11:48 -0300

    feat(setup): criacao do setup

---
+++ src/app/landingpage/landingpage.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeadingComponent } from './components/heading/heading.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeadingComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LandingpageModule { }
